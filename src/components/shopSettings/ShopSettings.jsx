e881d78166ee4f1cfd0402f564583fe67728d35b
Artur Tsebaev <tsebaevartur38@gmail.com>
2024-09-01 13:28:06 +0300

    fix adaptive styles

--- src/components/shopSettings/ShopSettings.jsx
+++ src/components/shopSettings/ShopSettings.jsx
@@ -5,8 +5,10 @@ import "./ShopSettings.css";
 export default function ShopSettings(props) {
   return (
     <div className="header">
+      <div className="header_content">
       <ProductSearch />
       <ProductsFiltres {...props} />
+      </div>
     </div>
   );
 }
